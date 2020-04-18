59c033053cd942eb11087c1a856ff65dab2ad0bd
Nick <barnard.nicholas@gmail.com>
2020-04-18 19:56:12 +0100

    test hosting

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "@reach/router";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <Container>
+      <Row>
+        <Col xs={12}>
+          <main>
+            <section className="card shadow">
+              <h1>Brain Sesh Home</h1>
+              <Link to="/styles">
+                <h4>Style Guide</h4>
+              </Link>
+              <Link to="/quizzes/0001">
+                <h4>Single Quiz</h4>
+              </Link>
+            </section>
+          </main>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
